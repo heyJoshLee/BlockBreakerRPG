fcc2309f076fa1ab4d86cbd6ad0b63b8a3a3f3a4
jleewebdev <lee45276@gmail.com>
2015-11-25 19:14:58 +0800

    Added music to start menu

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+	
+	static MusicPlayer instance = null;
+
+	// Use this for initialization
+	void Awake () {
+		if (instance != null) {
+			Destroy(gameObject);
+		} else {
+			instance = this;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
